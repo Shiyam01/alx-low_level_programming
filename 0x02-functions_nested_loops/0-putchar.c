08ab8cce064dcc2b682869f6dc757332dec5a133
Shiyam01 <shiiyam01@gmail.com>
2023-09-01 18:05:08 -0700

    puctar()function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+include "main.h"
+/**
+ * main - check desc 
+ * 
+ * desc : the program prints _putchar, followed by a new line
+ * rtn : 0
+ */
+int main(void)
+{
+	char ch = '_putchar';
+
+	_putchar(ch); 
+
+	return (0);
+}
