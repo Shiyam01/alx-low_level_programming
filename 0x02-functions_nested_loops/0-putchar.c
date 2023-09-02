d22191039e071e0cf28aebaa328d0be9e2f8c026
Shiyam01 <shiiyam01@gmail.com>
2023-09-02 04:22:28 -0700

    puchtar function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main - check desc 
-* desc : the program prints _putchar, followed by a new line
-* return : 0
-*/
+ * main - Entry point
+ *
+ * Description : the program prints _putchar, followed by a new line
+ * return : 0
+ */
 int main(void)
 {
-	char word[9] = "_puchtar";
-        int c;
-        for (c = 0; c < 9; c++)	
+	char word[9] = "_putchar";
+		int c;
+		for (c = 0; c < 9; c++)	
 		putchar(word[c]);
 	putchar('\n');
 	return (0);
