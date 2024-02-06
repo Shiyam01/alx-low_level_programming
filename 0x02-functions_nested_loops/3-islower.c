c513c4e028623d348cbbbf497088cc9c40bd4d4e
Shiyam01 <shiiyam01@gmail.com>
2024-02-06 06:40:37 -0800

    lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - function that checks for lowercase character
+ * _islower - a function that checks for lowercase character
  * @c : lowercase character
  * Return: 1 if c is lowercase, 0 otherwise
  */
